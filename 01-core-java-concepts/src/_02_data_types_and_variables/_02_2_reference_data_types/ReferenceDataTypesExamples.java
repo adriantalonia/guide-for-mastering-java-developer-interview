6440d6019ab42203d9b2d111bddd4b425e7a10f2
Adrian Talonia <adrian.talonia11@gmail.com>
2024-09-14 15:18:04 -0600

    add primitive data types and reference data types section

---
+++ 01-core-java-concepts/src/_02_data_types_and_variables/_02_2_reference_data_types/ReferenceDataTypesExamples.java
@@ -0,0 +1,76 @@
+package _02_data_types_and_variables._02_2_reference_data_types;
+
+public class ReferenceDataTypesExamples {
+
+    public static void main(String[] args) {
+        // Array Example
+        int[] numbers = {1, 2, 3, 4, 5};
+        System.out.println("Array Example:");
+        for (int number : numbers) {
+            System.out.println(number);
+        }
+
+        // Class and Object Example
+        CarExercise myCar = new CarExercise("Red", "Tesla Model S");
+        System.out.println("\nClass and Object Example:");
+        myCar.displayInfo();
+
+        // Interface Example
+        AnimalExercise myDog = new DogExercise();
+        System.out.println("\nInterface Example:");
+        myDog.sound();
+
+        // String Example
+        String greeting = "Hello, World!";
+        System.out.println("\nString Example:");
+        System.out.println("Length: " + greeting.length());
+        System.out.println("Character at index 1: " + greeting.charAt(1));
+        System.out.println("Substring from index 7: " + greeting.substring(7));
+
+        // Object Example with toString and equals
+        CarExercise anotherCar = new CarExercise("Blue", "BMW i8");
+        System.out.println("\nObject Example:");
+        System.out.println("Car details: " + anotherCar.toString());
+        System.out.println("Cars are equal: " + myCar.equals(anotherCar));
+    }
+}
+
+// Class Example
+class Car {
+    String color;
+    String model;
+
+    Car(String color, String model) {
+        this.color = color;
+        this.model = model;
+    }
+
+    void displayInfo() {
+        System.out.println("Car Model: " + model + ", Color: " + color);
+    }
+
+    @Override
+    public String toString() {
+        return "Car Model: " + model + ", Color: " + color;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        CarExercise car = (CarExercise) obj;
+        return color.equals(car.color) && model.equals(car.model);
+    }
+}
+
+// Interface Example
+interface Animal {
+    void sound();
+}
+
+// Implementing Interface Example
+class Dog implements AnimalExercise {
+    public void sound() {
+        System.out.println("Woof");
+    }
+}
---
+++ 01-core-java-concepts/src/_02_data_types_and_variables/_02_2_reference_data_types/ReferenceDataTypesExercises.java
@@ -0,0 +1,85 @@
+package _02_data_types_and_variables._02_2_reference_data_types;
+
+public class ReferenceDataTypesExercises {
+
+    public static void main(String[] args) {
+        // Array Exercise: Sum of all elements
+        int[] numbers = {1, 2, 3, 4, 5};
+        int sum = 0;
+        for (int number : numbers) {
+            sum += number;
+        }
+        System.out.println("Array Exercise: Sum of all elements = " + sum);
+
+        // Class and Object Exercise: Display car details
+        CarExercise myCar = new CarExercise("Red", "Tesla Model S");
+        System.out.println("\nClass and Object Exercise:");
+        myCar.displayInfo();
+
+        // Interface Exercise: Animal sounds
+        AnimalExercise myDog = new DogExercise();
+        AnimalExercise myCat = new Cat();
+        System.out.println("\nInterface Exercise:");
+        myDog.sound();
+        myCat.sound();
+
+        // String Exercise: Reverse a string
+        String original = "Hello, World!";
+        String reversed = new StringBuilder(original).reverse().toString();
+        System.out.println("\nString Exercise: Reversed string = " + reversed);
+
+        // Object Exercise: Check equality of two cars
+        CarExercise anotherCar = new CarExercise("Blue", "BMW i8");
+        CarExercise thirdCar = new CarExercise("Red", "Tesla Model S");
+        System.out.println("\nObject Exercise:");
+        System.out.println("First car equals second car: " + myCar.equals(anotherCar));
+        System.out.println("First car equals third car: " + myCar.equals(thirdCar));
+    }
+}
+
+// Class Example with a method to display information
+class CarExercise {
+    String color;
+    String model;
+
+    CarExercise(String color, String model) {
+        this.color = color;
+        this.model = model;
+    }
+
+    void displayInfo() {
+        System.out.println("Car Model: " + model + ", Color: " + color);
+    }
+
+    @Override
+    public String toString() {
+        return "Car Model: " + model + ", Color: " + color;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        CarExercise car = (CarExercise) obj;
+        return color.equals(car.color) && model.equals(car.model);
+    }
+}
+
+// Interface Example
+interface AnimalExercise {
+    void sound();
+}
+
+// Implementing Interface Example: Dog
+class DogExercise implements AnimalExercise {
+    public void sound() {
+        System.out.println("Dog says: Woof");
+    }
+}
+
+// Implementing Interface Example: Cat
+class Cat implements AnimalExercise {
+    public void sound() {
+        System.out.println("Cat says: Meow");
+    }
+}
